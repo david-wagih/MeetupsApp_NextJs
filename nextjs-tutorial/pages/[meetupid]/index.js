60ac6cd94812c7b0cff06813b402ff6cdf3e4af1
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-01-16 00:38:11 +0200

    adding some dummy data

--- nextjs-tutorial/pages/[meetupid]/index.js
+++ nextjs-tutorial/pages/[meetupid]/index.js
@@ -11,4 +11,37 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticPaths(){
+  return {
+
+    fallback: false, // false means that i have provided all the supported paths
+    // this also can be fetched by an Api or database
+    paths: [
+      {params: {meetupId: 'm1'}},
+      {params: {meetupId: 'm2'}},
+    ]
+  }
+}
+
+
+
+export async function getStaticProps(context) {
+  // fetch data for single meetup
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData : {
+        image="https://ychef.files.bbci.co.uk/976x549/p07zy3y6.jpg",
+        id : "m1",
+        title="First Meetup",
+        address="Cairo, Egypt",
+        description="this is an awesome meetup!"
+      }
+    }
+  }
+}
+
 export default MeetupDetails;
--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -19,8 +19,36 @@ const DUMMY_MEETUPS = [
   },
 ];
 
-function HomePage() {
-  return <MeetupList meetups={DUMMY_MEETUPS} />;
+function HomePage(props) {
+  return <MeetupList meetups={props.meetups} />;
 }
 
+// here is better when you want to access the request object
+// or data that changes very fast every second maybe and revalidate even can't help you
+// will not run on the build but always on the server after deployment
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+// fetch data from database or an Api
+// this code only runs on the server
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+// good for caching
+// here is faster better than serverside
+// during the build process
+// this is so important to get the data before calling the HomePage function
+export async function getStaticProps() {
+  //fetch data from an api or database
+  return {
+    props: {
+      meetups: DUMMY_MEETUPS,
+    },
+    revalidate: 1, // this is some time for next js
+  };
+}
 export default HomePage;
