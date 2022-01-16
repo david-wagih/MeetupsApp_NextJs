e309aec6cca805db8e77412e9d97af68f0ccfdcc
davidwagih <78867146+david-wagih@users.noreply.github.com>
2022-01-16 19:13:29 +0200

    adding some server side code

--- nextjs-tutorial/pages/index.js
+++ nextjs-tutorial/pages/index.js
@@ -1,27 +1,38 @@
+import { MongoClient } from "mongodb";
 import MeetupList from "../components/meetups/MeetupList";
 // our-domain.com/
-
-const DUMMY_MEETUPS = [
-  {
-    id: "m1",
-    title: "First Meetup",
-    image: "https://ychef.files.bbci.co.uk/976x549/p07zy3y6.jpg",
-    address: "Cairo, Egypt",
-    description: "this is a first meetup!",
-  },
-  {
-    id: "m2",
-    title: "Second Meetup",
-    image:
-      "https://thumbs.dreamstime.com/b/london-big-ben-houses-parliament-uk-49652572.jpg",
-    address: "London, England",
-    description: "this is a Second meetup!",
-  },
-];
 
 function HomePage(props) {
   return <MeetupList meetups={props.meetups} />;
 }
+
+// good for caching
+// here is faster better than serverside
+// during the build process
+// this is so important to get the data before calling the HomePage function
+export async function getStaticProps() {
+  //fetch data from an api or database
+  const client = await MongoClient.connect(
+    "mongodb+srv://david-wagih:Davidwagih123@cluster0.5gxme.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupCollection = db.collection("meetups");
+  const meetups = await meetupCollection.find().toArray();
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        image: meetup.image,
+        address: meetup.address,
+        id: meetup._id.toString(), // because it was a complex object
+      })),
+    },
+    revalidate: 1, // this is some time for next js
+  };
+}
+export default HomePage;
 
 // here is better when you want to access the request object
 // or data that changes very fast every second maybe and revalidate even can't help you
@@ -37,18 +48,3 @@ function HomePage(props) {
 //     },
 //   };
 // }
-
-// good for caching
-// here is faster better than serverside
-// during the build process
-// this is so important to get the data before calling the HomePage function
-export async function getStaticProps() {
-  //fetch data from an api or database
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-    revalidate: 1, // this is some time for next js
-  };
-}
-export default HomePage;
